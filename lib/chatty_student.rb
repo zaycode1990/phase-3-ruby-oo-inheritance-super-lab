a87febd2d2177692d3212170a013d260fd64ffbe
Isaiah <ijandicos90@gmail.com>
2022-07-22 18:14:30 -0400

    commit tree 41771245656595fd3c7f3c793310c033b27ea60e

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,12 +1,26 @@
 require "pry"
 
 class ChattyStudent < Student
-  def hello
-    super
-    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
-  end
+    attr_accessor :name, :programming_language
 
-  def raise_hand
-    10.times { super }
-  end
+    def initialize(name, programming_language)
+        @name = name
+        @programming_language = programming_language
+    end
+
+    def greeting(name, programming_language)
+        puts "Hello, #{name}"
+        puts "Welcome to the #{programming_language} curriculum"
+      end
+      
+      
+      def hello
+        super
+        puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    end
+    
+    def raise_hand
+        10.times { super }
+    end
 end
+
